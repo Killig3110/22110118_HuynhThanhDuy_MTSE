1153d05b8c13a111d9cf0e221192154f18f619d8
Killig3110 <killig.3110@gmail.com>
2025-11-05 09:53:10 +0700

    BT01

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1>👋 Xin chào, mình là Huỳnh Thanh Duy!</h1>
+        <p>🎓 Sinh viên năm cuối ngành Công nghệ phần mềm</p>
+      </header>
+
+      <section className="info">
+        <h2>📄 Thông tin cá nhân</h2>
+        <ul>
+          <li><b>MSSV:</b> 22110118</li>
+          <li><b>Lớp: 22110CLST02</b> </li>
+          <li><b>Email:</b> 22110118@student.hcmute.edu.vn</li>
+        </ul>
+      </section>
+
+      <footer>
+        <p>© 2025 – Huỳnh Thanh Duy | ReactJS by Vite ⚡</p>
+      </footer>
+    </div>
+  )
+}
+
+export default App
